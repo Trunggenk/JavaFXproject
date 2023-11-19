1accd2886e4b94f9f70ff60be269b0a12b423fc3
Trunggenk <22028170@vnu.edu.vn>
2023-11-20 01:33:15 +0700

    search and view

--- src/HashMapToListViewExample.java
+++ src/HashMapToListViewExample.java
@@ -1,63 +1,25 @@
-/* import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
+public class HashMapToListViewExample extends Application {
 
-public class HashMapToListViewExample  extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        WebView webView = new WebView();
+
+        // Load a HTML string
+        String htmlContent = "<html><body><h2>Hello, world!</h2></body></html>";
+        webView.getEngine().loadContent(htmlContent, "text/html");
+
+        Scene scene = new Scene(webView, 800, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-
-    @Override
-    public void start(Stage primaryStage) {
-        HashMap<String, String> dataMap = new HashMap<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("src/eng_vie.txt"));
-            String line;
-            while ( (line = br.readLine()) != null){
-                String[] str = line.split("<html>");
-                dataMap.put(str[0], str[1]);
-            }
-        }
-        catch (Exception e){
-            System.out.println("Error");
-        }
-
-        ObservableList<String> dataList = FXCollections.observableArrayList(dataMap.keySet());
-        FXCollections.sort(dataList);
-
-        ListView<String> listView = new ListView<>(dataList);
-
-        TextField searchField = new TextField();
-        searchField.setPromptText("Search...");
-
-        searchField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                ObservableList<String> filteredData = FXCollections.observableArrayList(dataList.filtered(item -> item != null && item.toLowerCase().startsWith(newValue.toLowerCase())));
-                FXCollections.sort(filteredData);
-                listView.setItems(filteredData);
-            }
-        });
-
-        VBox root = new VBox(searchField, listView);
-
-        Scene scene = new Scene(root, 300, 200);
-        primaryStage.setScene(scene);
-
-        primaryStage.setTitle("Search Bar Example");
-        primaryStage.show();
-    }
-
 }
-
- */
--- src/page1.java
+++ src/page1.java
@@ -1,14 +1,72 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.web.WebView;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.HashMap;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ResourceBundle;
 
 public class page1 {
 
+    @FXML
+    private ListView<String> ListWords;
 
-    public void searchFieldAction(KeyEvent keyEvent) {
+    @FXML
+    private TextField searchField;
+    @FXML
+    private WebView webdif;
+
+
+    private ObservableList<String> dataList;
+    private HashMap<String, String> dataMap;
+
+    public void initialize() {
+        dataMap = new HashMap<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src/eng_vie.txt"));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] str = line.split("<html>");
+                dataMap.put(str[0], str[1]);
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+
+        dataList = FXCollections.observableArrayList(dataMap.keySet());
+        FXCollections.sort(dataList);
+        ListWords.setItems(dataList);
+
+
+        ListWords.setOnMouseClicked(event -> {
+            String selectedWord = ListWords.getSelectionModel().getSelectedItem();
+            String htmlContent = dataMap.get(selectedWord);
+            webdif.getEngine().loadContent(htmlContent,"text/html");
+        });
+    }
+
+    @FXML
+    void searchFieldAction(KeyEvent event) {
+        String newValue = searchField.getText();
+        if (newValue != null) {
+            ObservableList<String> filteredData = FXCollections.observableArrayList(dataList.filtered(item -> item != null && item.toLowerCase().startsWith(newValue.toLowerCase())));
+            FXCollections.sort(filteredData);
+            ListWords.setItems(filteredData);
+        }
     }
 }
