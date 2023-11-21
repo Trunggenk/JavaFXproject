58d77b921cc61d69090d668c91637f4935db0e01
Trunggenk <22028170@vnu.edu.vn>
2023-11-21 12:48:59 +0700

    final 1-2 on working 3

---
+++ src/FlashcardController.java
@@ -0,0 +1,163 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+public class FlashcardController {
+    @FXML
+    private ListView<String> ListOfCard;
+    @FXML
+    private Button addAllfromhistory;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button start;
+    private int currentCardIndex = 0;
+
+
+    private List<Flashcard> cards = new ArrayList<>();
+
+    public void initialize() {
+        addAllfromhistory.setOnAction(event -> addAllFromHistory());
+        addButton.setOnAction(event -> addCard());
+        start.setOnAction(event -> startLearning()); // Add action for start button
+
+    }
+
+    private void addAllFromHistory() {
+        HashMap<String, String> dataMap = GlobalData.getDataMap();
+        List<String> history = readHistoryFromFile();
+
+        for (String front : history) {
+            String back = dataMap.get(front);
+            if (back != null) {
+                cards.add(new Flashcard(front, back));
+            }
+        }
+
+        updateListView();
+    }
+
+    private void addCard() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("New Card");
+        dialog.setHeaderText("Enter the front and back of the card:");
+        dialog.setContentText("Front:");
+        Optional<String> front = dialog.showAndWait();
+
+        dialog.setContentText("Back:");
+        Optional<String> back = dialog.showAndWait();
+
+        if (front.isPresent() && back.isPresent()) {
+            cards.add(new Flashcard(front.get(), back.get()));
+            updateListView();
+        }
+    }
+
+    private void updateListView() {
+        ObservableList<String> observableList = FXCollections.observableArrayList();
+        for (Flashcard card : cards) {
+            observableList.add(card.getFront());
+        }
+        ListOfCard.setItems(observableList);
+    }
+
+    private List<String> readHistoryFromFile() {
+        List<String> history = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/history.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                history.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return history;
+    }
+    private void startLearning() {
+        if (cards.isEmpty()) {
+            return;
+        }
+        if (currentCardIndex >= cards.size()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Completion Message");
+            alert.setHeaderText(null);
+            alert.setContentText("You have completed all the flashcards!");
+
+            alert.showAndWait();
+            return; // Exit the method if all flashcards have been viewed
+        }
+
+        Flashcard currentCard = cards.get(currentCardIndex);
+        Stage learningStage = new Stage();
+        learningStage.setHeight(400); // Set the height of the Stage
+        learningStage.setWidth(600); // Set the width of the Stage
+
+
+        VBox layout = new VBox();
+        Label frontLabel = new Label(currentCard.getFront());
+        frontLabel.setTextAlignment(TextAlignment.CENTER); // Center the text within the label
+        frontLabel.setAlignment(Pos.CENTER);
+        WebView backWebView = new WebView();
+        WebEngine webEngine = backWebView.getEngine();
+        webEngine.loadContent(currentCard.getBack());
+        backWebView.setVisible(false);
+        frontLabel.getStyleClass().add("label");
+
+        frontLabel.setOnMouseClicked(event -> {
+            backWebView.setVisible(true);
+        });
+
+        backWebView.setOnMouseClicked(event -> {
+            learningStage.close();
+            startLearning();
+        });
+
+        layout.getChildren().addAll(frontLabel, backWebView);
+        Scene scene = new Scene(layout);
+        scene.getStylesheets().add(getClass().getResource("flashcardview.css").toExternalForm());
+        learningStage.setScene(scene);
+        learningStage.show();
+
+        playSoundUS(currentCard.getFront());
+        currentCardIndex++;
+    }
+
+    private void playSoundUS(String selectedWord) {
+
+        if (selectedWord != null) {
+            String urlStr = "https://ssl.gstatic.com/dictionary/static/sounds/oxford/" + selectedWord + "--_us_1.mp3";
+            try {
+                URL url = new URL(urlStr);
+                HttpURLConnection huc = (HttpURLConnection) url.openConnection();
+                huc.setRequestMethod("HEAD");
+                int responseCode = huc.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    Media sound = new Media(urlStr);
+                    MediaPlayer mediaPlayer = new MediaPlayer(sound);
+                    mediaPlayer.play();
+                } else {
+                    throw new Exception("File not found");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/page3.java
+++ src/page3.java
@@ -1,4 +1,7 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
@@ -19,20 +22,23 @@ import javafx.scene.control.MenuItem;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.ResourceBundle;
 
 public class page3 {
     @FXML
-    private ListView<String> ListHistoryInPage3;
+    protected ListView<String> ListHistoryInPage3;
     @FXML
-    private Button crossword;
+    protected Button crossword;
     @FXML
-    private Button flashcard;
+    protected Button flashcard;
 
     public void initialize() {
         HashMap<String, String> dataMap = GlobalData.getDataMap();
         ListHistoryInPage3.setItems(GlobalData.getHistoryList());
-        flashcard.setOnAction(event -> createFlashcard());
+        flashcard.setOnAction(event -> openFlashcard());
+
         ContextMenu contextMenu = new ContextMenu();
 
         // Create a menu item
@@ -44,52 +50,10 @@ public class page3 {
 
         // Set context menu to the history list
         ListHistoryInPage3.setContextMenu(contextMenu);
-        ListHistoryInPage3.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
-            @Override
-            public ListCell<String> call(ListView<String> param) {
-                return new ListCell<String>() {
-                    @Override
-                    protected void updateItem(String item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item != null) {
-                            setText(item);
-                            // If this word is the selected word, set the text color to red
-                            if (item.equals(ListHistoryInPage3.getSelectionModel().getSelectedItem())) {
-                                setTextFill(Color.RED);
-                            } else {
-                                setTextFill(Color.BLACK);
-                            }
-                        } else {
-                            setText(null);
-                        }
-                    }
-                };
-            }
-        });
+
     }
 
-    private void createFlashcard() {
-        String selectedWord = ListHistoryInPage3.getSelectionModel().getSelectedItem();
-        if (selectedWord == null) {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Lỗi");
-            alert.setHeaderText(null);
-            alert.setContentText("Vui lòng chọn một từ để tạo flashcard");
-            alert.showAndWait();
-            return;
-        }
 
-        Stage flashcardStage = new Stage();
-        VBox layout = new VBox();
-        Text wordText = new Text(selectedWord);
-        Button flipButton = new Button("Lật flashcard");
-        flipButton.setOnAction(event -> wordText.setText(GlobalData.getDataMap().get(selectedWord)));
-        Button closeButton = new Button("Đóng flashcard");
-        closeButton.setOnAction(event -> flashcardStage.close());
-        layout.getChildren().addAll(wordText, flipButton, closeButton);
-        flashcardStage.setScene(new Scene(layout));
-        flashcardStage.show();
-    }
     private void removeWordFromHistory() {
         String selectedWord = ListHistoryInPage3.getSelectionModel().getSelectedItem();
         if (selectedWord != null) {
@@ -107,5 +71,16 @@ public class page3 {
             e.printStackTrace();
         }
     }
+    private void openFlashcard() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("flashcard.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
